0c84f1f550462f5a1eeaab9790e96ce396a7de54
jennifer toje <Jennifer>
2020-02-19 14:25:06 -0600

    Initial commit

---
+++ TicTacToe.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToe.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tic Tac Toe");
+
+            Console.WriteLine("Press ENTER to begin");
+            Console.ReadLine();
+        }
+    }
+}
